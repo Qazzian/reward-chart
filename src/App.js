7629d89ebe657c6eeeb6731fe9f0217af831fd8a
Qazzian <qazzian@gmail.com>
2018-11-16 10:44:32 +0000

    Copy the old App.js file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { Provider } from 'react-redux'
 import './App.css';
 
+import Header from './components/header/Header';
+import Footer from './components/Footer/Footer';
+import NewChartContainer from './components/newChart/NewChartContainer';
+import Today from './Today/Today';
+import Charts from './components/Charts';
+import More from './components/More';
+import AppState from './data/AppState';
+import ChartStore from './data/ChartStore';
+import {saveData, loadData} from './data/LocalStorage';
+
+import {
+	BrowserRouter,
+	Route,
+} from 'react-router-dom';
+
+const Routes = () => {
+	return (
+		<div className="main">
+			<Route path="/" exact={true} component={Today}/>
+			<Route path="/today" component={Today}/>
+			<Route path="/new" component={NewChartContainer}/>
+			<Route path="/charts" component={Charts}/>
+			<Route path="/more" component={More}/>
+		</div>
+	);
+};
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		const initialState = loadData();
+
+		this.store = ChartStore(AppState, initialState);
+		this.store.subscribe(() => {
+			saveData(this.store.getState());
+		})
+	}
+
+	render() {
+		return this.renderWithRoutes();
+	}
+
+	renderWithRoutes() {
+		return (
+			<Provider store={this.store}>
+				<BrowserRouter>
+					<div className="App">
+						<div className="container">
+							<Header/>
+							<Routes/>
+							<Footer/>
+						</div>
+					</div>
+				</BrowserRouter>
+			</Provider>
+		);
+	}
 }
 
 export default App;
