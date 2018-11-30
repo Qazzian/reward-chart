d33c68655ff0a6c51c437013b42f6a47b9e51775
Qazzian <Qazzian@gmail.com>
2018-11-30 02:51:00 +0000

    Refactor the Chart list

--- src/Today/TodayChartList.js
+++ src/Today/TodayChartList.js
@@ -2,20 +2,27 @@ import React from 'react';
 import ChartToday from './ChartToday/ChartToday';
 import NewChartContainer from '../components/newChart/NewChartContainer';
 
-const TodayChartList = (props) => {
-  if (props.charts.length === 0) {
-    return (
-      <>
-        <p>You have no charts yet.</p>
-        <p>Why not create one?</p>
-        <NewChartContainer />
-      </>
-    )
-  }
-
-	return props.charts.map((chart) => (
-		<ChartToday key={chart.name} chart={chart} onHappyClick={props.onHappyClick} onSadClick={props.onSadClick}/>
-	));
+export default function (props) {
+	if (props.charts.length === 0) {
+		return renderEmptyPage();
+	}
+	else {
+		return renderChartList(props);
+	}
 };
 
-export default TodayChartList;
+const renderEmptyPage = () => (
+	<>
+		<p>You have no charts yet.</p>
+		<p>Why not create one?</p>
+		<NewChartContainer />
+	</>
+);
+
+const renderChartList = (props) => props.charts.map((chart) => (
+	<ChartToday key={chart.name}
+		chart={chart}
+		onHappyClick={props.onHappyClick}
+		onSadClick={props.onSadClick}
+	/>
+));
