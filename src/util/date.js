051ea9524cb955d5b330153c93340808d0403f78
Qazzian <Qazzian@gmail.com>
2019-01-18 09:02:08 +0000

    Date Utils - subtractDays and loopOverDays

--- src/util/date.js
+++ src/util/date.js
@@ -12,3 +12,16 @@ export function toDateString(date) {
 		throw new Error(`expected Date object but got ${date}`)
 	}
 }
+
+export function subtractDays(date, days) {
+	const newDate = new Date(date);
+	newDate.setDate(newDate.getDate() - days);
+	return newDate;
+}
+
+export function loopOverDays(daysBack, callback, endDate=new Date()) {
+	for (let index = (daysBack-1); index >= 0; index--) {
+		const currentDate = subtractDays(endDate, index);
+		callback(currentDate);
+	}
+}
--- src/util/date.test.js
+++ src/util/date.test.js
@@ -13,4 +13,49 @@ describe('Date Utils', () => {
 			expect(toDateString(new Date('2018-12-14'))).toBe('2018-12-14');
 		});
 	});
+
+	describe('subtractDays', () => {
+		const {subtractDays} = dateUtil;
+		const tests = [
+			{
+				name: 'calc two days off',
+				startDate: '2018-12-14',
+				subtract: 2,
+				expected: '2018-12-12',
+			},
+			{
+				name: 'cross month boundries',
+				startDate: '2018-12-05',
+				subtract: 7,
+				expected: '2018-11-28',
+			},
+			{
+				name: 'Cross year boundries',
+				startDate: '2019-01-5',
+				subtract: 7,
+				expected: '2018-12-29',
+			},
+		];
+
+		tests.forEach((test) => {
+			it(`should ${test.name}`, () => {
+				const startDate = new Date(test.startDate);
+				const expectedDate = new Date(test.expected);
+				expect(subtractDays(startDate, test.subtract).toDateString())
+					.toMatch(expectedDate.toDateString());
+			});
+		});
+
+	});
+
+	describe('loopOverDays', () => {
+		const {loopOverDays} = dateUtil;
+
+		it('should iterate over the days', () => {
+			const spy = jest.fn((dateParam) => {});
+			loopOverDays(5, spy, new Date('2018-12-14'));
+			expect(spy.mock.calls.length).toBe(5);
+			expect(spy.mock.calls).toMatchSnapshot();
+		});
+	});
 });
